c494326beeda1fc96fc43c79d5197a0cd63341dd
Ramesh <rameshatal970@gmail.com>
2023-10-13 13:24:11 +0530

    Login with OTP added(Android)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_shoppacart/phone_auth/login_phone_page.dart';
 import 'package:firebase_shoppacart/user_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: UserPage(),
+      home: LoginPhonePage(),
     );
   }
 }
